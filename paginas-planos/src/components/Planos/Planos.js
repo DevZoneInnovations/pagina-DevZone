67a6c846aa3dc3b16b5b8a818d77f1587b616226
vini <marcosviniciuscanty@gmail.com>
2024-05-22 23:26:48 -0300

    pagina completa

---
+++ paginas-planos/src/components/Planos/Planos.js
@@ -0,0 +1,202 @@
+import "./Planos.css";
+
+function Planos() {
+  return (
+    <div className="plans" id="plans">
+      <div className="header">
+        <a href="https://devzoneinnovations.com.br/">Voltar</a>
+        <h1>Planos</h1>
+      </div>
+      <div className="grid-plans">
+        <div className="planoEssencialZone">
+          <div className="titulo-essencial">
+            <h3>Essential Zone</h3>
+            <p>
+              <b>R$ 500,00</b>
+            </p>
+          </div>
+          <ul>
+            <li>
+              <b className="texto-essential">
+                Este plano é ideal para sites simples e informativos, como sites
+                de pequenos negócios locais, portfólios pessoais, páginas de
+                currículo, sites de eventos únicos, entre outros.
+              </b>
+            </li>
+            <li>Desenvolvimento nas 3 etapas;</li>
+            <li>Suporte técnico por 30 dias após o lançamento;</li>
+            <li>Desenvolvimento com até 5 paginas;</li>
+            <li>Design simples e responsivo;</li>
+            <li>Atualizações mediante pagamento;</li>
+            <li>
+              <b className="negrito-essential">
+                Domínio garantido por 30 dias;
+              </b>
+            </li>
+          </ul>
+          <div className="frase-especial">
+            <a href="https://wa.me/5511912125307">
+              Se interessou? Nos chame no whatsapp
+            </a>
+          </div>
+        </div>
+        <div className="planoPremiumZone">
+          <div className="titulo-premium">
+            <h3>Premium Zone</h3>
+            <p>
+              <b>R$ 950,00</b>
+            </p>
+          </div>
+          <ul>
+            <li>
+              <b className="texto-premium">
+                Sites que se beneficiariam nesse plano incluem pequenas e médias
+                empresas que desejam uma presença online mais robusta. Isso pode
+                incluir sites institucionais com seções adicionais, como "Sobre
+                Nós", "Serviços", "Contato" e talvez um blog. Também pode ser
+                usado para sites de negócios locais ou pequenas lojas online com
+                um número limitado de produtos.
+              </b>
+            </li>
+            <li>Desenvolvimento nas 3 etapas;</li>
+            <li>Suporte técnico por 60 dias após o lançamento;</li>
+            <li>Desenvolvimento com até 10 paginas;</li>
+            <li>Design moderno e responsivo;</li>
+            <li>Atualizações mediante pagamento;</li>
+            <li>
+              <b className="negrito-premium">Domínio garantido por 60 dias;</b>
+            </li>
+          </ul>
+          <div className="frase-especial">
+            <a href="https://wa.me/5511912125307">
+              Se interessou? Nos chame no whatsapp
+            </a>
+          </div>
+        </div>
+        <div className="planoEliteZone">
+          <div className="titulo-elite">
+            <h3>Elite Zone</h3>
+            <p>
+              <b>R$ 1390,00</b>
+            </p>
+          </div>
+          <ul>
+            <li>
+              <b className="texto-elite">
+                Este plano é mais adequado para empresas maiores ou que precisam
+                de um site altamente personalizado com funcionalidades
+                avançadas. Isso inclui lojas online com um grande catálogo de
+                produtos, sites de empresas de médio a grande porte.
+              </b>
+            </li>
+            <li>Desenvolvimento nas 3 etapas;</li>
+            <li>Suporte técnico por 3 meses após o lançamento;</li>
+            <li>Desenvolvimento com até 20 paginas;</li>
+            <li>Design moderno e responsivo;</li>
+            <li>Atualizações garantidas por 90 dias após o lançamento;</li>
+            <li>
+              <b className="negrito-elite">Domínio garantido por 90 dias;</b>
+            </li>
+          </ul>
+          <div className="frase-especial">
+            <a href="https://wa.me/5511912125307">
+              Se interessou? Nos chame no whatsapp
+            </a>
+          </div>
+        </div>
+        <div className="planoGuardianZone">
+          <div className="titulo-guardian">
+            <h3>Guardian Zone</h3>
+            <p>
+              <b>R$ 90,00</b>
+            </p>
+          </div>
+          <ul>
+            <li>
+              <b className="texto-guardian">
+                Este plano é destinado a qualquer tipo de site que precise de
+                alguma atualização. Isso inclui todos os tipos de sites, desde
+                pequenos sites informativos até grandes lojas online. A
+                manutenção contínua é crucial para garantir que o site permaneça
+                seguro, atualizado e funcional ao longo do tempo.
+              </b>
+            </li>
+            <li>Atualizaçoes no sistema de acordo com suas necessidades;</li>
+            <li>Novo planejamento seguindo o Desenvolvimento 3 etapas;</li>
+            <li>Renovação de mais 30 dias de suporte técnico;</li>
+          </ul>
+          <div className="frase-especial">
+            <a href="https://wa.me/5511912125307">
+              Se interessou? Nos chame no whatsapp
+            </a>
+          </div>
+        </div>
+        <div className="planoBubbleZone">
+          <div className="titulo-bubble">
+            <h3>Bubble Zone</h3>
+            <p>
+              <b>R$ 50,00</b>
+            </p>
+          </div>
+          <ul>
+            <li>
+              <b className="texto-bubble">
+                Este plano é ideal para pessoas que desejam um link totalmente
+                personalizado para suas redes sociais. A personalização é feita
+                de acordo com seus gostos e estilo, refletindo perfeitamente sua
+                identidade pessoal ou a da sua empresa.
+              </b>
+            </li>
+            <li>
+              Criação de um
+              <b className="negrito-bubble"> LinkMe personalizado</b> para suas
+              redes sociais;
+            </li>
+            <li>Desenvolvimento nas 3 etapas;</li>
+            <li>Suporte técnico por 30 dias após o lançamento;</li>
+            <li>Atualizações mediante pagamento;</li>
+          </ul>
+          <div className="frase-especial">
+            <a href="https://wa.me/5511912125307">
+              Se interessou? Nos chame no whatsapp
+            </a>
+          </div>
+        </div>
+        <div className="planoCreativeZone">
+          <div className="titulo-creative">
+            <h3>Creative Zone</h3>
+            <p>
+              <b>R$ 45,00</b>
+            </p>
+          </div>
+          <ul>
+            <li>
+              <b className="texto-creative">
+                Este plano é ideal para transformar a identidade visual da sua
+                empresa com nossas logotipos inovadores e únicos que capturam a
+                essência do seu negócio. Nossos designs são pensados para
+                destacar sua marca no mercado e atrair o público certo.
+              </b>
+            </li>
+            <li>
+              Criação de três
+              <b className="negrito-creative"> Logotipos personalizadas</b> para
+              suas redes sociais;
+            </li>
+            <li>Desenvolvimento nas 3 etapas;</li>
+
+            <li>Suporte técnico por 30 dias após o lançamento;</li>
+            <li>Atualizações mediante pagamento;</li>
+          </ul>
+          <div className="frase-especial">
+            <a href="https://wa.me/5511912125307">
+              Se interessou? Nos chame no whatsapp
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Planos;
